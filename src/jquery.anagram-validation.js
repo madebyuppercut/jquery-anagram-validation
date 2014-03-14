e41e00ceb4cf6b7c03656f6ff4c66f8efd54d6be
Weslie Leung <wleung@madebyuppercut.com>
2014-03-14 12:23:00 -0600

    Remove version number from source

--- src/jquery.anagram-validation.js
+++ src/jquery.anagram-validation.js
@@ -1,5 +1,5 @@
 /**
- * Anagram Validation jQuery Plugin v0.1
+ * Anagram Validation jQuery Plugin
  *
  * The purpose of this plugin is enable client-side validation (it's up to the application to determine its CSS) as weel as server-side error message rendering.
  * For usage, please refer to the Jasmine test suite and fixture.
